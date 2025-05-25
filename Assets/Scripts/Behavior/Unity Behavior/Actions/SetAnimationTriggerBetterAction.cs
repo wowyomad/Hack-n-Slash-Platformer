8117e93e422cf648a499f8910dbd0a3a8c3588c4
wowyomad <vadimsun8837@gmail.com>
2025-05-25 21:59:38 +0300

    Stun animation

--- Assets/Scripts/Behavior/Unity Behavior/Actions/SetAnimationTriggerBetterAction.cs
+++ Assets/Scripts/Behavior/Unity Behavior/Actions/SetAnimationTriggerBetterAction.cs
@@ -5,12 +5,11 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "SetAnimationTriggerBetter", story: "Trigger animation [Trigger] in [Agent]", category: "Action/Animation", id: "edb4c3333d00e2a2d3a2ff178efc5bb1")]
+[NodeDescription(name: "SetAnimationTriggerBetter", story: "Trigger [Trigger] in [Agent] animator", category: "Action", id: "edb4c3333d00e2a2d3a2ff178efc5bb1")]
 public partial class SetAnimationTriggerBetterAction : Action
 {
     [SerializeReference] public BlackboardVariable<string> Trigger;
     [SerializeReference] public BlackboardVariable<GameObject> Agent;
-
     private int m_TriggerHash;
     private Animator m_Animator;
     private bool m_Initialized = false;
