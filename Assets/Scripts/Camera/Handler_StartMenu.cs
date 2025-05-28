91bcd3558bfa529cf3d455d9d5f98904fde1f724
wowyomad <vadimsun8837@gmail.com>
2025-05-28 14:03:32 +0300

    Level list populated!

--- Assets/Scripts/Camera/Handler_StartMenu.cs
+++ Assets/Scripts/Camera/Handler_StartMenu.cs
@@ -1,55 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
 using TheGame;
+using TMPro;
+using Unity.AppUI.UI;
 using UnityEngine;
 
-public class Handler_StartMenu : MonoBehaviour
+namespace TheGame
 {
-    [Header("UI Elements")]
-    public GameObject LevelListContent;
-    public GameObject ChallengeListContent;
-    public GameObject LevelName;
-    public GameObject LevelDescription;
-
-
-    [Header("Data")]
-    public LevelPresetsData LevelPresetsData;
-    public LevelSaveData LevelSaveData;
-
-
-    [Header("UI Prefabs")]
-    public GameObject LevelButtonPrefab;
-    public GameObject ChallengePrefab;
-
-    private void Awake()
+    public class Handler_StartMenu : MonoBehaviour
     {
-        if (LevelListContent == null || ChallengeListContent == null || LevelName == null || LevelDescription == null)
+        [Header("UI Elements")]
+        public GameObject LevelListContentRef;
+        public GameObject ChallengeListContentRef;
+        public GameObject LevelNameRef;
+        public GameObject LevelDescriptionRef;
+
+        [Header("UI Prefabs")]
+        public GameObject LevelButtonPrefab;
+        public GameObject ChallengePrefab;
+
+        private LevelManager m_LevelManager;
+        private List<Level> m_Levels;
+        private GameSaveData m_SaveData;
+        private List<GameObject> m_LevelButtons;
+
+
+        private void Awake()
         {
-            Debug.LogError("UI elements are not assigned in the Handler_StartMenu script.");
+            if (LevelListContentRef == null || ChallengeListContentRef == null || LevelNameRef == null || LevelDescriptionRef == null)
+            {
+                Debug.LogError("UI elements are not assigned in the Handler_StartMenu script.");
+            }
+
+            if (LevelButtonPrefab == null || ChallengePrefab == null)
+            {
+                Debug.LogError("UI prefabs are not assigned in the Handler_StartMenu script.");
+            }
+
+            if (!TryGetComponent<LevelManager>(out m_LevelManager))
+            {
+                Debug.LogError("LevelManager component not found.");
+            }
         }
 
-        if (LevelButtonPrefab == null || ChallengePrefab == null)
+        private void Start()
         {
-            Debug.LogError("UI prefabs are not assigned in the Handler_StartMenu script.");
+            //Must be called after LevelManager start!
+            m_SaveData = m_LevelManager.SaveData;
+            m_Levels = m_LevelManager.RuntimeLevels;
+
+            PopulateLevelList();
+            PopulateChallengeList();
         }
 
-        if (LevelPresetsData == null || LevelSaveData == null)
+        private void PopulateLevelList()
         {
-            Debug.LogError("Data objects are not assigned in the Handler_StartMenu script.");
+            m_LevelButtons = m_Levels
+                .Select(level =>
+                {
+                    GameObject levelButtonGameObject = Instantiate(LevelButtonPrefab, LevelListContentRef.transform);
+
+                    TMP_Text levelButtonText = levelButtonGameObject.GetComponentInChildren<TMP_Text>();
+                    levelButtonText.text = level.Name;
+
+                    return levelButtonGameObject;
+
+                })
+                .ToList();
         }
-    }
 
-    private void Start()
-    {
-        PopulateLevelList();
-        PopulateChallengeList();
-    }
+        private void PopulateChallengeList()
+        {
 
-    private void PopulateLevelList()
-    {
-
-    }
-
-    private void PopulateChallengeList()
-    {
-        
+        }
     }
 }
